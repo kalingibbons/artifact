426dd7a7f6d13b0522592b1c9ae0125799798e83
Kalin Gibbons <kalingibbons@gmail.com>
2020-11-18 17:43:08 -0700

    Create helpers module

--- notebooks/02-kg-model-selection.py
+++ notebooks/02-kg-model-selection.py
@@ -9,19 +9,12 @@
 # %%
 import os
 import sys
-import json
-import pickle
 import math
 import logging
 from pathlib import Path
 from IPython.display import display
 
-from colorama import Fore, Style
 import numpy as np
-import scipy as sp
-import scipy.io as spio
-import statsmodels.api as sm
-from statsmodels.formula.api import ols
 
 import sklearn
 from sklearn.ensemble import (
@@ -49,6 +42,7 @@ import pandas as pd
 
 import artifact
 from artifact.datasets import load_tkr, tkr_group_lut
+from artifact.helpers import RegressionProfile
 
 
 # %%
@@ -65,58 +59,6 @@ pd.set_option("display.max_rows", 120)
 pd.set_option("display.max_columns", 120)
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-
-# %% [markdown]
-# ## Class and function definitions
-
-# %%
-
-
-class RegressionProfile:
-    def __init__(self, load_path=None):
-        self.error_dataframes = dict()
-        if load_path:
-            try:
-                self.load(load_path)
-            except FileNotFoundError:
-                pass
-
-    def __repr__(self):
-        keys = self.error_dataframes.keys()
-        if keys is not None:
-            return f'RegressionProfile object with keys {list(keys)}'
-        else:
-            return 'Uninitialized RegressionProfile object'
-
-    def add_results(self, name, error_dataframe):
-        self.error_dataframes[name] = error_dataframe
-
-    def save(self, save_path):
-        with open(save_path, 'wb') as file:
-            pickle.dump(self.error_dataframes, file, pickle.HIGHEST_PROTOCOL)
-
-    def load(self, load_path):
-        with open(load_path, 'rb') as file:
-            self.error_dataframes = pickle.load(file)
-
-    def summarize(self, name):
-        try:
-            df = self.error_dataframes[name]
-        except KeyError:
-            print(f'No error summary with key {name} was found.')
-            return
-        best_learners = df.idxmin()
-        print(1 * '\n')
-        print(Fore.YELLOW + name + '\n' + len(name) * '-')
-        # print(len(name) * '-')
-        print(Style.RESET_ALL)
-        print('Best learners total by response:')
-        display(best_learners.value_counts(), best_learners.sort_values())
-        print('\n\nSorted by median RMS error (smallest to largest):')
-        display(df.T.describe().T.sort_values(by=['50%']))
-        print('\n\nRMS Errors:')
-        display(df)
-        print(2 * '\n')
 
 
 # %% [markdown]
---
+++ src/artifact/helpers.py
@@ -0,0 +1,53 @@
+# %%
+import pickle
+from colorama import Fore, Style
+from IPython.display import display
+
+# %%
+
+
+class RegressionProfile:
+    def __init__(self, load_path=None):
+        self.error_dataframes = dict()
+        if load_path:
+            try:
+                self.load(load_path)
+            except FileNotFoundError:
+                pass
+
+    def __repr__(self):
+        keys = self.error_dataframes.keys()
+        if keys is not None:
+            return f'RegressionProfile object with keys {list(keys)}'
+        else:
+            return 'Uninitialized RegressionProfile object'
+
+    def add_results(self, name, error_dataframe):
+        self.error_dataframes[name] = error_dataframe
+
+    def save(self, save_path):
+        with open(save_path, 'wb') as file:
+            pickle.dump(self.error_dataframes, file, pickle.HIGHEST_PROTOCOL)
+
+    def load(self, load_path):
+        with open(load_path, 'rb') as file:
+            self.error_dataframes = pickle.load(file)
+
+    def summarize(self, name):
+        try:
+            df = self.error_dataframes[name]
+        except KeyError:
+            print(f'No error summary with key {name} was found.')
+            return
+        best_learners = df.idxmin()
+        print(1 * '\n')
+        print(Fore.YELLOW + name + '\n' + len(name) * '-')
+        # print(len(name) * '-')
+        print(Style.RESET_ALL)
+        print('Best learners total by response:')
+        display(best_learners.value_counts(), best_learners.sort_values())
+        print('\n\nSorted by median RMS error (smallest to largest):')
+        display(df.T.describe().T.sort_values(by=['50%']))
+        print('\n\nRMS Errors:')
+        display(df)
+        print(2 * '\n')
